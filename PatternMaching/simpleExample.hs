4037006c4570c38d9cc16bc99ccbc5fcdf7de284
Shalitha Suranga <shalithasuranga@gmail.com>
2017-08-10 18:48:53 +0530

    new hs file added

---
+++ PatternMaching/simpleExample.hs
@@ -0,0 +1,22 @@
+{-
+
+Author - Shalitha Suranga
+www.fb.com/shalithasuranga
+
+-}
+
+-- type definition 
+
+eight :: (Integral a) => a -> String
+
+
+ -- list of patters each one will give different result
+
+eight 1 = "No! it's not eight!"
+eight 2 = "No! it's not eight!"
+eight 3 = "No! it's not eight!"
+eight 4 = "No! it's not eight!"
+eight 5 = "No! it's not eight!"
+eight 6 = "No! it's not eight!"
+eight 7 = "No! it's not eight! but lucky seven"
+eight 8 = "Yes it's eight!"
